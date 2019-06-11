39ff40a3b7bebd3f8b5aa3c2ee09aa1271ee42c5
Wesley Berrêdo <wesley.a@samsung.com>
2019-06-11 12:40:56 -0400

    feat(app): get data from latitude and longitude and calculate geohash

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'foundation-sites/dist/css/foundation.min.css';
+import * as geohash  from 'ngeohash';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: '',
+      longitude: '',
+      hash: '',
+    };
+
+    this.latitudeChanged = this.latitudeChanged.bind(this);
+    this.longitudeChanged = this.longitudeChanged.bind(this);
+  }
+
+  latitudeChanged(event) {
+    this.setState({
+      latitude: event.target.value,
+    });
+
+    this.calculateHash();
+  }
+
+  longitudeChanged(event) {
+    this.setState({
+      longitude: event.target.value,
+    });
+
+    this.calculateHash();
+  }
+
+  calculateHash() {
+    const { latitude, longitude } = this.state;
+
+    if (latitude && longitude) {
+      const hash = geohash.encode(latitude, longitude);
+
+      this.setState({ hash });
+    }
+  }
+
+  render() {
+    const { state } = this;
+
+    return (
+      <div className="App">
+        <form className="form">
+          <h4> Geohash Converter</h4>
+
+          <label>
+            Latitude
+            <input
+              type="number"
+              placeholder="Latitude"
+              aria-describedby="Latitude" 
+              value={state.latitude}
+              onChange={this.latitudeChanged}
+            />
+          </label>
+
+          <label>
+            Longitude
+            <input
+              type="number"
+              placeholder="Longitude"
+              aria-describedby="Longitude" 
+              value={state.longitude}
+              onChange={this.longitudeChanged}
+            />
+          </label>
+
+          <label>Geohash: {state.hash}</label>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
